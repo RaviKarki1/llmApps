203dbf20832e7c0d2e1a3e1b49f772c4cc430a5e
RAVI KARKI <ravi@RAVIs-MacBook-Air.local>
2026-03-03 09:32:47 +1100

    upload form to upload resume and job description

---
+++ frontend/src/components/upload/FileUploadBox.jsx
@@ -0,0 +1,18 @@
+function FileUploadBox({label, onFileSelect}){
+    return(
+       <div>
+        <label>{{label}}</label>
+        <input 
+            type="file"
+            accept=".pdf, .docx"
+            onChange={(e) => {
+                if(e.target.files && e.target.files[0]){
+                    onFileSelect(e.target.files[0])
+                }
+            }}
+        />
+       </div>
+    )
+}
+
+export default FileUploadBox;
